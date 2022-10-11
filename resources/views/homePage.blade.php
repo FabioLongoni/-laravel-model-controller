180e6dfceb08bb32e28ce121fa11c87fc4feffc4
FabioLongoni <longoni.fabio90@gmail.com>
2022-10-11 17:19:23 +0200

    check

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -1,16 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <div>
+@extends('layouts.app');
+
+@section('metaTitle','My-Movies');
+
+@section('content')
+  <div class="container">
     @foreach ($movies as $movie)
-        <h1>{{ $movie->title }}</h1>
+      <div class="info__sx">
+        <h2>{{$movie->title}}</h2>
+        <h3>{{$movie->original_title}}</h3>
+      </div>
+      <div class="info__dx">
+        <p>{{$movie->nationality}}</p>
+        <p>{{$movie->vote}}</p>
+        <p>{{$movie->date}}</p>
+      </div>     
     @endforeach
   </div>
-</body>
-</html>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+  <div>
+    <footer>
+      <h4>
+        Selected by BooleanCareers
+      </h4>
+    </footer>
+  </div>
+</footer>
